7a494aac1fb7ba4c3101a28b6ad124ebbfd999b0
Horyzonty Nauki <horyzontynauki.kontakt@gmail.com>
2025-10-18 13:49:48 +0200

    fix: return Supabase client from supabase-browser

--- lib/supabase-browser.ts
+++ lib/supabase-browser.ts
@@ -2,8 +2,9 @@
 
 import { createClient } from '@supabase/supabase-js';
 
-export const supabaseBrowser = () =>
-  createClient(
+export const supabaseBrowser = () => {
+  return createClient(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
     process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
   );
+};
